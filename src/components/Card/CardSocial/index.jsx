ce2e257225dc2b82dd3bbeba32cedfdf49c9b7cd
Unknown <carlos15erh@gmail.com>
2021-04-04 23:57:46 -0500

    adding social media
    instagram replaced for facebook

--- src/components/Card/CardSocial/index.jsx
+++ src/components/Card/CardSocial/index.jsx
@@ -4,17 +4,14 @@ import './styles.css';
 export const CardSocial = () => {
   return (
     <div className='card_social'>
-      <a href='#'>
+      <a href='https://twitter.com/crehds1' target='_blank'>
         <img src='../../assets/images/twitter.png' alt='logo de twitter' />
       </a>
-      <a href='#'>
+      <a href='https://github.com/crehds' target='_blank'>
         <img src='../../assets/images/github.png' alt='logo de github' />
       </a>
-      <a href='#'>
-        <img
-          src='../../assets/images/instagram.png'
-          alt='logo de instagram'
-        />
+      <a href='https://www.facebook.com/checa.rh' target='_blank'>
+        <img src='../../assets/images/facebook.png' alt='logo de instagram' />
       </a>
     </div>
   );
