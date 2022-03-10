bc3fede643559c171ea9cb98e277269427ceecd9
carlos rodriguez <carlos15erh@gmail.com>
2022-03-10 01:28:01 -0500

    email js added

--- src/components/Content/components/Contact/index.jsx
+++ src/components/Content/components/Contact/index.jsx
@@ -5,6 +5,9 @@ import './styles.css';
 export function Contact() {
   function onSubmit(e) {
     e.preventDefault();
+    emailjs
+      .sendForm('service_xcf4f4j', 'template_nqy8aeh', '#contact-form')
+      .then(() => console.log('success'));
   }
   return (
     <div className='Contact'>
@@ -23,20 +26,24 @@ export function Contact() {
           </a>
         </p>
       </div>
-      <form action='' onSubmit={onSubmit}>
+      <form action='' onSubmit={onSubmit} id='contact-form'>
         <div>
           <label htmlFor=''>Nombre</label>
-          <input type='text' placeholder='Nombre' />
+          <input type='text' placeholder='Nombre' name='from_name' />
         </div>
 
         <div>
           <label htmlFor=''>E-mail</label>
-          <input type='email' placeholder='Correo electrónico' />
+          <input
+            type='email'
+            placeholder='Correo electrónico'
+            name='from_email'
+          />
         </div>
 
         <div>
           <label htmlFor=''>Mensaje</label>
-          <textarea name='' id='' cols='30' rows='10'></textarea>
+          <textarea name='message' id='' cols='30' rows='10'></textarea>
         </div>
         <div>
           <button type='submit'>Enviar</button>
