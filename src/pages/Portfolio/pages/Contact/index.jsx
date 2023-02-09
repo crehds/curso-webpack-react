81670e995e36099de2eae0e5a675b07c85cde68a
Carlos Rodriguez <carlos15erh@gmail.com>
2023-02-09 14:09:32 -0500

    refactor:  add and implements Form component in Contact page

--- src/pages/Portfolio/pages/Contact/index.jsx
+++ src/pages/Portfolio/pages/Contact/index.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Form } from '../Home/components/Form';
 import { Header } from '../Home/components/Header';
 import { Modal } from './components/Modal';
 import { ErrorModal } from './components/Modal/components/ErrorModal';
@@ -40,49 +41,13 @@ function Contact() {
     }
   }
 
-  function onSubmit(e) {
-    e.preventDefault();
-    setIsOpen(true);
-    handleEmail();
-  }
-
   return (
     <div className='Contact'>
       <Header />
-      <form action='' onSubmit={onSubmit} id='contact-form'>
-        <div>
-          <label htmlFor=''>Nombre</label>
-          <input type='text' placeholder='Nombre' name='from_name' />
-        </div>
-
-        <div>
-          <label htmlFor=''>E-mail</label>
-          <input
-            type='email'
-            placeholder='Correo electrónico'
-            name='from_email'
-          />
-        </div>
-
-        <div>
-          <label htmlFor=''>Teléfono</label>
-          <input type='tel' placeholder='Número telefónico' name='from_phone' />
-        </div>
-
-        <div>
-          <label htmlFor=''>Mensaje</label>
-          <textarea
-            name='message'
-            id=''
-            cols='30'
-            rows='8'
-            placeholder='Déjame un mensaje...'
-          ></textarea>
-        </div>
-        <div>
-          <button type='submit'>Enviar</button>
-        </div>
-      </form>
+      <Form
+        handleEmail={handleEmail}
+        setIsOpen={setIsOpen}
+      />
       {isOpen && (
         <Modal
           modalState={{ ...modalState }}
