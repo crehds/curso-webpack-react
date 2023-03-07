9999c62c183456b223287482dfb569724d5deecc
Carlos Rodriguez <carlos15erh@gmail.com>
2023-03-06 20:42:59 -0500

    add listener to handle theme preference

--- src/pages/Portfolio/context/theme.js
+++ src/pages/Portfolio/context/theme.js
@@ -1,11 +1,12 @@
 import {
-  createContext, useContext, useState
+  createContext, useContext, useState, useEffect
 } from "react";
 
 const ThemeContext = createContext();
 
 export function ThemeProvider ({ children }) {
-  const [theme, setTheme] = useState('light');
+  const getCurrentTheme = () => window.matchMedia("(prefers-color-scheme: dark)").matches ? 'dark' : 'light';
+  const [theme, setTheme] = useState(getCurrentTheme());
 
   function changeTheme(newTheme){
     if(newTheme === theme) return null;
@@ -13,6 +14,17 @@ export function ThemeProvider ({ children }) {
     element.classList.remove(theme)
     setTheme(newTheme);
   }
+
+  const themeListener = (event) => {
+    const newPreferColor = event.matches ? 'dark' : 'light';
+    setTheme(newPreferColor);
+  }
+
+  useEffect(() => {
+    const themePreference = window.matchMedia("(prefers-color-scheme: dark)");
+    themePreference.addEventListener('change', themeListener)
+    return () => themePreference.removeEventListener('change', themeListener);
+  }, [])
 
   return (
     <ThemeContext.Provider value={{ theme, changeTheme }}>
