f9fc57b1ce8ceb48663850620729ecebf125c644
Carlos Rodriguez <carlos15erh@gmail.com>
2023-03-08 17:08:56 -0500

    add debounce to handle the app's width

--- src/hooks/useGetWidth.js
+++ src/hooks/useGetWidth.js
@@ -1,7 +1,7 @@
 import {
   useCallback, useEffect, useState
 } from 'react';
-// import { debounce } from 'lodash';
+import useDebounce from './useDebounce';
 
 export function useGetWidth({ id = '' }) {
   const element = document.getElementById(id);
@@ -12,12 +12,17 @@ export function useGetWidth({ id = '' }) {
     setWidth(newWidth);
   }, [id]);
 
+  const handleWidth = () => {
+    const app = document.querySelector('.portfolio');
+    app.style.minWidth = `${width}px`;
+  }
+
+  const { debounceEffectHandler } = useDebounce(onResize, handleWidth, width, 750);
+
   useEffect(() => {
-    window.addEventListener('resize', onResize);
+    window.addEventListener('resize', debounceEffectHandler);
     return () => {
-      return window.removeEventListener('resize', onResize);
+      return window.removeEventListener('resize', debounceEffectHandler);
     };
   }, []);
-
-  return width;
 }
