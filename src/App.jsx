c087d3115aecacbb6bc008d006c5ed85ecd6d7a9
Carlos Rodriguez <carlos15erh@gmail.com>
2023-03-08 17:12:40 -0500

    add debounce to handle the app's width

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,10 @@
-import { useEffect } from "react";
 import { useGetWidth } from "./hooks/useGetWidth";
 import { Loader1 } from "./loaders/loader1";
 import { Portfolio } from "./pages/Portfolio";
 import { ThemeProvider } from "./pages/Portfolio/context/theme";
 
 const App = () => {
-  const width = useGetWidth({ id: 'app' });
-
-  useEffect(() => {
-    const app = document.querySelector('.portfolio');
-    app.style.minWidth = `${width}px`;
-  }, [width]);
+  useGetWidth({ id: 'app' });
 
   return (
     <ThemeProvider>
