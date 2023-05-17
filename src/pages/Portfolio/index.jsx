5f874d1a6abba40136e31402e4ce054a986abe38
Carlos Rodriguez <carlos15erh@gmail.com>
2023-05-17 18:10:01 -0500

    persistent location with location context

--- src/pages/Portfolio/index.jsx
+++ src/pages/Portfolio/index.jsx
@@ -1,4 +1,4 @@
-import { Suspense, useState } from 'react';
+import { Suspense } from 'react';
 import { useActiveOptionNavBar } from '../../hooks/useActiveOptionNavbar';
 import { useGetData } from '../../hooks/useGetData';
 import { Loader1 } from '../../loaders/loader1';
@@ -7,24 +7,25 @@ import { NavBar } from './components/Navbar';
 import './styles/portfolio.css';
 import { Content } from './containers/Content';
 import { useTheme } from './context/theme';
+import { useLocation } from './context/location';
 
 export function Portfolio(props) {
-  const [page, setPage] = useState('home');
-  const setOption = useActiveOptionNavBar(page);
   const [user, loading] = useGetData(true);
   const { theme } = useTheme();
+  const { location, changeLocation } = useLocation();
+  const setOption = useActiveOptionNavBar(location);
 
   return (
     <Suspense fallback={<Loader1 />}>
       <section className={`portfolio ${theme}`}>
         <NavBar
-          appContent={page}
-          setAppContent={setPage}
+          appContent={location}
+          setAppContent={changeLocation}
           setActiveOptionNavBar={setOption}
         />
         {loading && props.onLoading()}
         <Content
-          page={page}
+          page={location}
           user={user} />
       </section>
     </Suspense>
