acf80b9961459c06221f32d6abae2855143c9f3f
Carlos Rodriguez <carlos15erh@gmail.com>
2023-05-20 19:57:08 -0500

    remove paragraph in about me page

--- src/pages/Portfolio/pages/About/components/InfoParagraph/index.jsx
+++ src/pages/Portfolio/pages/About/components/InfoParagraph/index.jsx
@@ -1,14 +1,9 @@
-import { useTranslation } from 'react-i18next';
 import './styles.css';
 import TECHNOLOGIES from '../../consts/technologies';
 
 export function InfoParagraph() {
-  const { t }= useTranslation();
   return (
     <div className='InfoParagraph'>
-      <p>
-        {t('portfolio.pages.about.info-paragraph')}
-      </p>
       <div className='technologies'>
         {
           TECHNOLOGIES.map((technology) => (
